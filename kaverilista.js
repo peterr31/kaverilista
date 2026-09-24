fa8dd2ee84cac3410ba8b8e67445872697b113ca
Pete <eemelihuuskonen137@gmail.com>
2026-09-24 10:34:46 +0300

    parannuksia

--- kaverilista.js
+++ kaverilista.js
@@ -1,11 +1,6 @@
 
 let nykyinen_kaveri_lista = []
 
-function tyhjennä_kaveri_lista() {
-    nykyinen_kaveri_lista = []
-    päivitä_lista()
-}
-
 function päivitä_lista() {
     let ul = document.getElementById("kaveri_lista");
     ul.innerHTML = "";
@@ -16,15 +11,17 @@ function päivitä_lista() {
     }
 }
 
-function lisää_kaveri_v2() {
-    let nimi = document.getElementById("nimi_input").value;
-    if (nimi !== "") {
-        nykyinen_kaveri_lista.push(nimi);
-        päivitä_lista() 
-        document.getElementById("nimi_input").value = "";
-    }
+function resetoi_placeholder() {
+    let input = document.getElementById("nimi_input");
+    input.placeholder = "Syötä nimi:"
+    input.style.color = "rgb(238, 238, 238)";
 }
 
+function aseta_placeholder(text, color = "rgb(0, 128, 0)") {
+    let input = document.getElementById("nimi_input");
+    input.placeholder = text;
+    input.style.color = color;
+}
 function lisää_kaveri_v1() {
     nykyinen_kaveri_lista = []
     let array = [];
@@ -47,17 +44,47 @@ function lisää_kaveri_v1() {
     }
 }
 
-function poista_kaveri() {
-    let nimi = document.getElementById("nimi_input").value;
-    let index = nykyinen_kaveri_lista.indexOf(nimi);
-    if (index !== -1) {
-        nykyinen_kaveri_lista.splice(index, 1);
+function lisää_kaveri_v2() {
+    let input = document.getElementById("nimi_input");
+    let nimi = input.value;
+
+    if (nimi === "") {
+        aseta_placeholder("NIMI EI VOI OLLA TYHJÄ", "rgb(255, 0, 0)");
+    } else {
+        nykyinen_kaveri_lista.push(nimi);
         päivitä_lista();
-        document.getElementById("nimi_input").value = "";
+        input.value = "";
+        aseta_placeholder("Lisätty onnistuneesti!");
+    }
+}
+
+function poista_kaveri() {
+    let input = document.getElementById("nimi_input");
+    let nimi = input.value;
+
+    if (nimi === "") {
+        aseta_placeholder("NIMI EI VOI OLLA TYHJÄ","rgb(255, 0, 0)")
+    } else {
+        let index = nykyinen_kaveri_lista.indexOf(nimi);
+        if (index !== -1) {
+            nykyinen_kaveri_lista.splice(index, 1);
+            päivitä_lista();
+            input.value = "";
+            aseta_placeholder("Poistettu onnistuneesti!")
+        } else {
+            alert("Nimeä ei löytynyt. Varmista, että nimi on kirjoitettu oikein");
+        }
     }
 }
 
 function järjestä_kaverit() {
     nykyinen_kaveri_lista.sort();
     päivitä_lista();
+    resetoi_placeholder();
+}
+
+function tyhjennä_kaveri_lista() {
+    nykyinen_kaveri_lista = [];
+    päivitä_lista();
+    resetoi_placeholder();
 }
